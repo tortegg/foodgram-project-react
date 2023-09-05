f31fb1108d9d4d9856f98c0f11c632f0d7a11ef3
Антон Кудрин <antokudrin4682@yandex.ru>
2023-09-05 15:27:37 +0300

    fix isort troubles

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -1,8 +1,7 @@
 import csv
 
-from django.core.management.base import BaseCommand
 from django.conf import settings
-
+from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
