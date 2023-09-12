7ce00141f5440466f5110fb9983b5bae16646831
Антон Кудрин <antokudrin4682@yandex.ru>
2023-09-12 10:33:15 +0300

    fix review 2

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -39,6 +39,10 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return self.username
 
